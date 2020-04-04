d24540f872d8ac53edd94e51129c4ede36cf8b7b
Sidhant <sdsd011235@gmail.com>
2020-04-04 23:59:24 +0530

    sticky header implemented

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, HostListener } from '@angular/core';
+import { Component, AfterViewInit, HostBinding } from '@angular/core';
 import { fromEvent } from 'rxjs';
 import {
   distinctUntilChanged,
@@ -9,24 +9,100 @@ import {
   throttleTime
 } from 'rxjs/operators';
 
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger
+} from '@angular/animations';
+
+enum Direction {
+  Up = 'Up',
+  Down = 'Down'
+}
+
+
+enum VisibilityState {
+  Visible = 'visible',
+  Hidden = 'hidden'
+}
 
 @Component({
   selector: 'ncov-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  animations: [
+    trigger('toggle', [
+      state(
+        VisibilityState.Hidden,
+        style({ opacity: 0, transform: 'translateY(-100%)' })
+      ),
+      state(
+        VisibilityState.Visible,
+        style({ opacity: 1, transform: 'translateY(0)' })
+      ),
+      transition('* => *', animate('200ms ease-in'))
+    ])
+  ]
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements AfterViewInit {
 
-  scrolled = false;
+  // private isVisible = true;
+  // constructor() { }
 
-  @HostListener('window:scroll', ['$event'])
-  onWindowScroll($event) {
-      this.scrolled = $event.srcElement.scrollTop >= 150;
-      console.log(this.scrolled);
+  // ngAfterViewInit() {
+  //   const scroll$ = fromEvent(window, 'scroll').pipe(
+  //     throttleTime(10),
+  //     map(() => window.pageYOffset),
+  //     pairwise(),
+  //     map(([y1, y2]): Direction => (y2 < y1 ? Direction.Up : Direction.Down)),
+  //     distinctUntilChanged(),
+  //     share()
+  //   );
+  //   console.log(scroll$);
+
+  //   const scrollUp$ = scroll$.pipe(
+  //     filter(direction => direction === Direction.Up)
+  //   );
+  //   const scrollDown$ = scroll$.pipe(
+  //     filter(direction => direction === Direction.Down)
+  //   );
+  //   scrollUp$.subscribe(() => (this.isVisible = true));
+  //   scrollDown$.subscribe(() => (this.isVisible = false));
+
+  // }
+
+  private isVisible = true;
+
+  @HostBinding('@toggle')
+  get toggle(): VisibilityState {
+    return this.isVisible ? VisibilityState.Visible : VisibilityState.Hidden;
   }
-  constructor() { }
 
-  ngOnInit() {
+  ngAfterViewInit() {
+    const scroll$ = fromEvent(window, 'scroll').pipe(
+      throttleTime(10),
+      map(() => window.pageYOffset),
+      pairwise(),
+      map(([y1, y2]): Direction => (y2 < y1 ? Direction.Up : Direction.Down)),
+      distinctUntilChanged(),
+      share()
+    );
+    console.log(scroll$);
+
+    const goingUp$ = scroll$.pipe(
+      filter(direction => direction === Direction.Up)
+    );
+
+    const goingDown$ = scroll$.pipe(
+      filter(direction => direction === Direction.Down)
+    );
+
+    goingUp$.subscribe(() => (this.isVisible = true));
+    goingDown$.subscribe(() => (this.isVisible = false));
+
+    console.log(this.isVisible);
   }
 
 }
