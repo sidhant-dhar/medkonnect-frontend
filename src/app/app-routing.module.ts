41cee0cdf4e184cb9833685a47684b6e72c93614
Valindo Fiell Godinho <valindo.godinho@gmail.com>
2020-04-18 13:58:13 +0000

    Added About us page - generated component using ng g - Added images for the entire page in /assets/images/ - Added flex based styles to each section and also verified on mobile.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import {HomepageComponent} from './homepage/homepage.component';
 import {BuildppeComponent } from './buildppe/buildppe.component';
 import {NeedppeComponent} from './needppe/needppe.component';
 import {HaveppeComponent} from './haveppe/haveppe.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 const routes: Routes = [
 
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: HomepageComponent},
   {path: 'build', component: BuildppeComponent},
   {path: 'need', component: NeedppeComponent},
-  {path: 'have', component: HaveppeComponent}
+  {path: 'have', component: HaveppeComponent},
+  {path: 'about', component: AboutUsComponent}
 ];
 
 @NgModule({
